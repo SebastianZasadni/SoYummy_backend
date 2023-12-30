8149fa7bca35a757feddaa279a32936cad03309c
SebastianZasadni <emblelele@gmail.com>
2023-12-30 11:41:31 +0100

    fix uploadImage response

--- controllers/recipes/uploadImage.js
+++ controllers/recipes/uploadImage.js
@@ -2,10 +2,13 @@ const { handleUpload } = require('../../middleware/multer');
 
 const uploadImage = async (req, res) => {
     try {
+        if (!req.file) {
+            throw new Error("Image didn't upload.");
+        }
         const b64 = Buffer.from(req.file.buffer).toString("base64");
         let dataURI = "data:" + req.file.mimetype + ";base64," + b64;
         const cldRes = await handleUpload(dataURI);
-        res.json(cldRes);
+        res.json(cldRes.url);
     } catch (error) {
         console.log(error);
         res.send({
