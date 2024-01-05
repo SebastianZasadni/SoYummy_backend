c0f467ad3ef2d667fb4c3784fffef40242132cc0
SebastianZasadni <emblelele@gmail.com>
2024-01-05 09:07:49 +0100

    fix addRecipe ingredients

--- controllers/recipes/addRecipe.js
+++ controllers/recipes/addRecipe.js
@@ -2,12 +2,13 @@ const Recipe = require('../../models/recipe');
 
 const addRecipe = async (req, res, next) => {
     try {
-        const { title, about, category, thumb, time, preparation, ingredientsList
+        const { title, about, category, thumb, time, preparation, ingredients
         } = req.body;
+       
         const { _id: owner } = req.user;
         const newRecipe = await Recipe.create({
             title, category, instructions: preparation, description: about, thumb, preview: thumb, time,
-            ingredients: ingredientsList, owner
+            ingredients, owner
         });
         res.status(200).json({
             status: "success",
