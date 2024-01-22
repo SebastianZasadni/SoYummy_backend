f2ce1209466c695ad8c76eefdff47f53652fb4bf
SebastianZasadni <emblelele@gmail.com>
2024-01-22 18:28:09 +0100

    fix current response

--- controllers/auth/current.js
+++ controllers/auth/current.js
@@ -2,7 +2,7 @@ const User = require('../../models/user');
 
 const current = async (req, res, next) => {
     try {
-        const { email, name, id, token } = req.user;
+        const { email, name, id, token, thumb } = req.user;
         return res.status(200).json({
             id, name, email, token, thumb
         })
